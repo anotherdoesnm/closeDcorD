90e01d4486f037c0caa839121d47e258656609c4
doesnm <doesnm@0ut0f.space>
2025-07-04 11:47:40 +0300

    rename user_settings to settings

--- app/src/main/java/com/xinto/opencord/network/response/ApiLoginResult.kt
+++ app/src/main/java/com/xinto/opencord/network/response/ApiLoginResult.kt
@@ -6,5 +6,5 @@ import com.xinto.opencord.network.response.base.ApiResponse
 data class ApiLoginResult(
     @SerializedName("token") val token: String,
     @SerializedName("mfa") val mfa: Boolean = false,
-    @SerializedName("user_settings") val user_settings: ApiLoginUserSettingsResult,
+    @SerializedName("settings") val user_settings: ApiLoginUserSettingsResult,
 ) : ApiResponse
